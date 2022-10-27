7106bcbcd4dc1edf0bb1f1c2742d2ec442c54c34
Felix <ident_green@posteo.de>
2022-10-28 00:11:53 +0200

    minar changes, adaption to linux

--- server_statistics/main.py
+++ server_statistics/main.py
@@ -2,31 +2,32 @@ import os.path
 import time
 import datetime
 import numpy as np
-from server_statistics import api_handler
+# from server_statistics import api_handler
+import api_handler
 import pandas as pd
 import os
 
-
-path_wd = os.getcwd()# + "\\server_statistics\\"
+#path_wd = os.getcwd()# + "\\server_statistics\\"
+path_wd = "/home/pi/work/api_transport/server_statistics/"
 
 t1 = time.time()
+now = datetime.datetime.now()
+print("================== start script (" + str(now) + ") =====================")
 
 ext = 900070401  # "Tauernallee Santisstrasse"
 ext_dir = 900070301  # U Alt-Mariendorf
 
-
 pd.set_option('display.max_columns', 9)
 
-
-
-
 # read data from earlier run and remove files that do not match ext, ext_dir, "_h" and ".csv"
-files = os.listdir(path_wd + "\\records\\hourly\\")
+#files = os.listdir(path_wd + "\\records\\hourly\\")
+files = os.listdir(path_wd + "/records/hourly/")
 var = [((("s" + str(ext) + "_sDir" + str(ext_dir)) in f) & ("_h" in f) & (".csv" in f)) for f in files]
 files = np.array(files, dtype=str)[var]
 
 if len(files)>0:
-    data = pd.read_csv(path_wd + "\\records\\hourly\\" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
+    #data = pd.read_csv(path_wd + "\\records\\hourly\\" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
+    data = pd.read_csv(path_wd + "/records/hourly/" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
 else:
     data = pd.DataFrame()
 
@@ -77,9 +78,9 @@ for j in range(100):
 
         # save files in 3 different time scales to make sure, not all data is deleted.
         # delete hourly and daily saves from time to time.
-        data.to_csv("records/hourly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + "_h" + str(now)[11:13] + ".csv",sep=";")
-        #data.to_csv("records/daily/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + ".csv",sep=";")
-        # data.to_csv("records/monthly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:7] + ".csv", sep=";")
+        data.to_csv(path_wd + "records/hourly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + "_h" + str(now)[11:13] + ".csv",sep=";")
+        #data.to_csv(path_wd + "records/daily/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + ".csv",sep=";")
+        # data.to_csv(path_wd + "records/monthly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:7] + ".csv", sep=";")
 
     except Exception as e:
         print(" - Error in Request:", e)
