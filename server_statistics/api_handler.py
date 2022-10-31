1785377e3a227cb8ff57e6f5c2926af3f1a2e1f3
Felix Fauer <fauer.felix@posteo.de>
2022-10-31 21:24:06 +0100

    added second station

--- server_statistics/api_handler.py
+++ server_statistics/api_handler.py
@@ -53,7 +53,7 @@ def nameToExtStation(name):
     return id  # ['@extId']
 
 
-def nextDeparturesAtStop(name=False, ext=0, maxNo=3, ext_dir=0):
+def nextDeparturesAtStop(name=False, ext=0, maxNo=3, duration=20, ext_dir=False):
     if not ext:
         if name:
             ext = nameToExtStation(name)
@@ -62,15 +62,30 @@ def nextDeparturesAtStop(name=False, ext=0, maxNo=3, ext_dir=0):
 
     command = 'stops/' + str(ext) + '/' + 'departures?'
     if ext_dir:
-        param = {'direction': ext_dir, 'results': maxNo, 'duration': 20}
+        param = {'direction': ext_dir, 'results': maxNo, 'duration': duration}
     else:
-        param = {'results': maxNo, 'duration': 20}
+        param = {'results': maxNo, 'duration': duration}
     response = getData(command, param)
     return response
 
+def prettyPrint_departures(nextDep):
+    for i in range(len(nextDep)):  # for each departure event in this request
+
+        # extract information
+        iLine = nextDep[i]['line']['name']
+        iDest = nextDep[i]['direction']  # [0:12]
+        iTime_plan = nextDep[i]['plannedWhen']
+        iTime = nextDep[i]['when']
+        iRideID = nextDep[i]['line']['fahrtNr']  # HAS A BUG. X76 always with same ID
+
+        print(iTime, iLine, iDest)
+
 
 ext = 900070401  # "Tauernallee Santisstrasse"
+ext = 900003201  # "s+U Berlin Hauptbahnhof"
 ext_dir = 900070301  # U Alt-Mariendorf
+
+
 
 def handler_debug(ext=ext, maxNo=10, ext_dir=ext_dir):
     # if api is down, use this artificial data for debugging
@@ -78,3 +93,7 @@ def handler_debug(ext=ext, maxNo=10, ext_dir=ext_dir):
              'direction': 'Walter-Schreiber-Platz',
              'when': '2022-10-26T23:20:00'}
             for i in range(maxNo)]
+
+# debug test
+#x = nextDeparturesAtStop(ext=ext, duration=5, maxNo=30)
+#prettyPrint_departures(x)
--- server_statistics/main.py
+++ server_statistics/main.py
@@ -8,28 +8,38 @@ import pandas as pd
 import os
 
 #path_wd = os.getcwd()# + "\\server_statistics\\"
-path_wd = "/home/pi/work/api_transport/server_statistics/"
+#path_wd = "/home/pi/work/api_transport/server_statistics/"
+path_wd = "D:\\implementations\\api_transport\\server_statistics\\"
 
 t1 = time.time()
 now = datetime.datetime.now()
 print("================== start script (" + str(now) + ") =====================")
 
-ext = 900070401  # "Tauernallee Santisstrasse"
-ext_dir = 900070301  # U Alt-Mariendorf
+ext1 = 900070401  # "Tauernallee Santisstrasse"
+ext_dir1 = 900070301  # U Alt-Mariendorf
+ext2 = 900003201  # "s+U Berlin Hauptbahnhof"
+ext_dir2 = False
+
+ext_list=[ext1, ext2]
+ext_dir_list=[ext_dir1, ext_dir2]
+data = [None, None]
 
 pd.set_option('display.max_columns', 9)
 
 # read data from earlier run and remove files that do not match ext, ext_dir, "_h" and ".csv"
 #files = os.listdir(path_wd + "\\records\\hourly\\")
-files = os.listdir(path_wd + "/records/hourly/")
-var = [((("s" + str(ext) + "_sDir" + str(ext_dir)) in f) & ("_h" in f) & (".csv" in f)) for f in files]
-files = np.array(files, dtype=str)[var]
-
-if len(files)>0:
-    #data = pd.read_csv(path_wd + "\\records\\hourly\\" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
-    data = pd.read_csv(path_wd + "/records/hourly/" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
-else:
-    data = pd.DataFrame()
+files_raw = os.listdir(path_wd + "/records/hourly/")
+print("all files",files_raw)
+for station_i in range(len(data)):
+    var = [((("s" + str(ext_list[station_i]) + "_sDir" + str(ext_dir_list[station_i])) in f) & ("_h" in f) & (".csv" in f)) for f in files_raw]
+    print("var", var)
+    files = np.array(files_raw, dtype=str)[var]
+    print(station_i, files)
+    if len(files)>0:
+        #data = pd.read_csv(path_wd + "\\records\\hourly\\" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
+        data[station_i] = pd.read_csv(path_wd + "/records/hourly/" + files[len(files) - 1], sep=";", index_col=0, dtype=str)
+    else:
+        data[station_i] = pd.DataFrame()
 
 j=0
 while True:
@@ -38,53 +48,60 @@ while True:
     print("  - begin of loop (" + str(j) + "). Time: ", time.time() - t1)
     t_loop = time.time()
 
-    try:
-        nextDep = api_handler.nextDeparturesAtStop(ext=ext, maxNo=10, ext_dir=ext_dir)
+    for station_i in range(len(ext_list)):
+        ext, ext_dir = ext_list[station_i], ext_dir_list[station_i]
+        print("------------- \n handle station nr", station_i,"(",ext,")")
+        #print("content of loaded file:")
+        #print(data[data_i])
+        try:
+            nextDep = api_handler.nextDeparturesAtStop(ext=ext, maxNo=30, duration=5, ext_dir=ext_dir)
 
-        now = datetime.datetime.now()
-        data_dummy = []
+            now = datetime.datetime.now()
+            data_dummy = []
 
-        for i in range(len(nextDep)):  # for each departure event in this request
+            for i in range(len(nextDep)):  # for each departure event in this request
 
-            # extract information
-            iLine = nextDep[i]['line']['name']
-            iDest = nextDep[i]['direction']  # [0:12]
-            iTime_plan = nextDep[i]['plannedWhen']
-            iTime = nextDep[i]['when']
-            iRideID = nextDep[i]['line']['fahrtNr']  # HAS A BUG. X76 always with same ID
+                # extract information
+                iLine = nextDep[i]['line']['name']
+                iDest = nextDep[i]['direction']  # [0:12]
+                iTime_plan = nextDep[i]['plannedWhen']
+                iTime = nextDep[i]['when']
+                iRideID = nextDep[i]['line']['fahrtNr']  # HAS A BUG. X76 always with same ID
 
-            # handle dates and delay
-            iTime_plan_data = datetime.datetime.strptime(iTime_plan[0:19], '%Y-%m-%dT%H:%M:%S')
-            iTime_date = datetime.datetime.strptime(iTime[0:19], '%Y-%m-%dT%H:%M:%S')
-            iDelay_date = iTime_date - iTime_plan_data
-            if iDelay_date.seconds > 24 * 60 * 60 / 2:
-                iDelay = - (24 * 60 * 60 - iDelay_date.seconds) / 60
-            else:
-                iDelay = str(iDelay_date.seconds / 60)
+                # handle dates and delay
+                iTime_plan_data = datetime.datetime.strptime(iTime_plan[0:19], '%Y-%m-%dT%H:%M:%S')
+                iTime_date = datetime.datetime.strptime(iTime[0:19], '%Y-%m-%dT%H:%M:%S')
+                iDelay_date = iTime_date - iTime_plan_data
+                if iDelay_date.seconds > 24 * 60 * 60 / 2:
+                    iDelay = - (24 * 60 * 60 - iDelay_date.seconds) / 60
+                else:
+                    iDelay = str(iDelay_date.seconds / 60)
 
-            # add row for this data point
-            data_dummy.append({"time_plan": iTime_plan, "line": iLine, "dest": iDest, "delay": iDelay,
-                               "request": str(now)[0:19], "rideID": iRideID})
+                # add row for this data point
+                data_dummy.append({"time_plan": iTime_plan, "line": iLine, "dest": iDest, "delay": iDelay,
+                                   "request": str(now)[0:19], "rideID": iRideID})
 
-        # combine old and new data and remove duplicates
-        data_new = pd.DataFrame(data_dummy)
-        #print("data new\n", data_new)
-        data = pd.concat([data, data_new], ignore_index=True)
-        #print("combined old data + new data\n", data)
-        data = data.drop_duplicates(keep="last", subset=["time_plan", "rideID"])#, inplace=True)
-        data = data.sort_values(by="time_plan", axis=0)
-        #dups = data.duplicated(keep="last", subset=["time_plan", "rideID"])
-        #data_noDups = data[dups]
-        #print("combined data without duplicates\n", data)
+            #print(data_dummy)
+            # combine old and new data and remove duplicates
+            data_new = pd.DataFrame(data_dummy)
+            #print("data new\n", data_new)
+            data[station_i] = pd.concat([data[station_i], data_new], ignore_index=True)
+            #print("combined old data + new data\n", data[station_i])
+            data[station_i] = data[station_i].drop_duplicates(keep="last", subset=["time_plan", "rideID"])#, inplace=True)
+            data[station_i] = data[station_i].sort_values(by="time_plan", axis=0)
+            #print("combined data without duplicates\n", data)
 
-        # save files in 3 different time scales to make sure, not all data is deleted.
-        # delete hourly and daily saves from time to time.
-        data.to_csv(path_wd + "records/hourly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + "_h" + str(now)[11:13] + ".csv",sep=";")
-        #data.to_csv(path_wd + "records/daily/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + ".csv",sep=";")
-        # data.to_csv(path_wd + "records/monthly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:7] + ".csv", sep=";")
+            # save files in 3 different time scales to make sure, not all data is deleted.
+            # delete hourly and daily saves from time to time.
+            print("found",len(nextDep),"results. Store them in csv. print first new data:")
+            print(iTime, iLine, iDest,iDelay, iRideID)
+            data[station_i].to_csv(path_wd + "records/hourly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + "_h" + str(now)[11:13] + ".csv",sep=";")
+            #data.to_csv(path_wd + "records/daily/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:10] + ".csv",sep=";")
+            # data.to_csv(path_wd + "records/monthly/" + "s" + str(ext) + "_sDir" + str(ext_dir) + "_d" + str(now)[0:7] + ".csv", sep=";")
 
-    except Exception as e:
-        print(" - Error in Request:", e)
+        except Exception as e:
+            print(" - Error in Request:", e)
+
     time.sleep(3 * 60)
     #time.sleep(3)
     j=j+1
